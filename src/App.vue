0634f431e65d5bc868668113369c63efbf42e3bd
ArtanisCen <1543946889@qq.com>
2022-04-13 17:29:13 +0800

    最终

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="todoapp">
+    <TodoHeader @create="createFn" :arr="list" ></TodoHeader>
+    <TodoMain :arr="showArr" @del="deleteFn"></TodoMain>
+    <TodoFooter :farr="showArr" @changeType="typeFn" @clear="clearF"></TodoFooter>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "./styles/base.css"
+import "./styles/index.css"
 
+import TodoHeader from "./components/TodoHeader"
+import TodoMain from "./components/TodoMain"
+import TodoFooter from "./components/TodoFooter"
 export default {
-  name: 'App',
+  data () {
+    return {
+       list: JSON.parse(localStorage.getItem('todoList')) || [],
+      getSel:'all'
+    }
+  },
   components: {
-    HelloWorld
+    TodoHeader,
+    TodoMain,
+    TodoFooter,
+
+},
+  methods: {
+    createFn(taskName){
+      let id = this.list.length === 0 ? 100 : this.list[this.list.length - 1].id+1
+      this.list.push({
+        id:id,
+        name:taskName,
+        isDone:false
+      })
+    },
+    deleteFn(theid){
+      let index = this.list.findIndex(obj => obj.id === theid)
+      this.list.splice(index,1)
+    },
+    typeFn(str){
+      this.getSel = str
+    },
+    clearF(){
+      this.list=this.list.filter(obj=>obj.isDone==false)
+    }
+    
+  },
+  computed:{
+    showArr(){
+      if(this.getSel === "yes"){
+        return this.list.filter(obj => obj.isDone === true)
+      }else if (this.getSel === 'no'){
+        return this.list.filter(obj=>obj.isDone === false)
+      }else{
+        return this.list
+      }
+    }
+  },
+  watch: {
+    list:{
+      deep:true,
+      handler(){
+        localStorage.setItem('todoList',JSON.stringify(this.list))
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  devServer: { // 自定义服务配置
+    open: true,
+    port: 8081
+  },
+  lintOnSave: false // 关闭eslint检查
+}
